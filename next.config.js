99551d36b02ac42c866fce79379aef3f5141da98
Walter <walter.spades@gmail.com>
2023-12-21 17:28:00 +0800

    Fixing deployment again

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = nextConfig;
 module.exports = {
-    nextConfig,
     eslint: {
         ignoreDuringBuilds: true,
     },
-
 };
