3517bba34c3c855fceb855860174e0f619c82d4d
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2023-12-23 18:46:04 +0800

    bugfix

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -15,7 +15,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form action={createInvoice}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Customer Name */}
+        Customer Name
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
@@ -40,7 +40,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </div>
 
-        {/* Invoice Amount */}
+        Invoice Amount
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
             Choose an amount
@@ -60,7 +60,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           </div>
         </div>
 
-        {/* Invoice Status */}
+        Invoice Status
         <fieldset>
           <legend className="mb-2 block text-sm font-medium">
             Set the invoice status
