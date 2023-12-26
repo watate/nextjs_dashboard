d51432d900cdec5d1a8e36f8dbc45a4d6f71483b
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2023-12-26 18:40:04 +0800

    added 404 not found

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(
     { params }: { params: { id: string } }
@@ -10,6 +11,11 @@ export default async function Page(
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+
+    if (!invoice) {
+        notFound();
+    }
+
     return (
         <main>
             <Breadcrumbs
